1c3647b98b66b51bb1fe1bd693bece6b0e8e4364
anikmollik <anikmollik122321@gmail.com>
2023-09-01 01:48:21 -0700

    added blog.html

--- js/index.js
+++ js/index.js
@@ -1,29 +1,53 @@
-const handleCetagory = async() => {
-    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
-    const data = await response.json();
-    console.log(data.data)
-    const manu = data.data
-    const tabContainer = document.getElementById('tab-container');
-    manu.forEach((categorys) => {
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <a onClick="handleLoadNews('${categorys.category_id}')" class="tab">${categorys.category}</a> 
-        `
-        tabContainer.appendChild(div)
-        
-    });
-    
+const handleCetagory = async () => {
+  const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+  const data = await response.json();
+  const manu = data.data;
+  const tabContainer = document.getElementById('tab-container');
+
+  const handleClick = (categoryId, button) => {
+      // Remove inline styles from all category buttons
+      const allCategoryButtons = document.querySelectorAll('.tab');
+      allCategoryButtons.forEach((button) => {
+          button.removeAttribute('style');
+      });
+
+      // Change background and text color of the clicked category button
+      button.style.backgroundColor = '#007BFF'; // Change to the color you want
+      button.style.color = 'white'; // Change to the color you want
+
+      // Call handleLoadNews with the category ID
+      handleLoadNews(categoryId);
+  };
+
+  manu.forEach((category) => {
+      console.log(category.category_id);
+      const div = document.createElement('div');
+      const categoryButton = document.createElement('a');
+      categoryButton.classList.add('tab');
+      categoryButton.textContent = category.category;
+      categoryButton.addEventListener('click', () => {
+          handleClick(category.category_id, categoryButton);
+      });
+
+      div.appendChild(categoryButton);
+      tabContainer.appendChild(div);
+      
+  });
 }
 
 const handleLoadNews = async (categoryId) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
 
     const data = await response.json();
-
+    
+    // const result = data
     const cardContainer = document.getElementById('card-container')
+    cardContainer.innerHTML = ''
 
     data.data.forEach((news) => {
-        console.log(news.others.views)
+      console.log(news.others.views)
+
+        // console.log(news.authors[0].verified) 
         const div = document.createElement('div')
         div.innerHTML = `
 
@@ -38,6 +62,7 @@ const handleLoadNews = async (categoryId) => {
           <div class="">
             
             <p>${news.authors[0].profile_name}</p> 
+            <span">${news.authors[0].verified}</span>
             <p>${news.others.views}</p>
           </div>
         </div>
@@ -45,6 +70,7 @@ const handleLoadNews = async (categoryId) => {
 
         
         `
+        
         cardContainer.appendChild(div)
     })
 }
@@ -52,3 +78,4 @@ const handleLoadNews = async (categoryId) => {
 // const categories = data.data.map(item => item.category);
 // categories.forEach(category => console.log(category));
 handleCetagory()
+handleLoadNews(1000)
